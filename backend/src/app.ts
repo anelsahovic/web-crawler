81528a1f43773d29b1ebccb964b2a5d35c10743e
Anel <anel.sahovic.bsc@gmail.com>
2025-07-12 16:45:06 +0200

    Init express app with TypeScript

---
+++ backend/src/app.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction, Request, Response } from 'express';
+import createHttpError, { isHttpError } from 'http-errors';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('✅ Received GET / request');
+
+  res.send('Project is running!');
+});
+
+app.use((req, res, next) => {
+  next(createHttpError(404, 'Endpoint Not Found!'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occurred';
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
+
+export default app;
